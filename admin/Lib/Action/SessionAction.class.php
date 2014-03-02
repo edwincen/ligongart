978149c5c23bae07eb644c26cad1d416fabe8981
Vincent <275713271@qq.com>
2014-03-02 23:08:06 +0800

    session action and html add

---
+++ admin/Lib/Action/SessionAction.class.php
@@ -0,0 +1,24 @@
+<?php 
+class SessionAction extends Action{
+	
+	//查找所属级列表
+	public function index(){
+		$this->display();
+	}
+	
+	//添加所属级
+	public function add(){
+		
+	}
+	
+	//更新所属级
+	public function update(){
+		
+	}
+	
+	//删除所属级
+	public function delete(){
+		
+	}
+}
+?>
