bfb12a1766498bb0067a853b00838df42fd7eccb
Kexiang Cen <edwincen@yahoo.com.cn>
2014-03-03 01:37:00 +0800

    yes

---
+++ Apps/Lib/Action/ShenbaoAction.class.php
@@ -0,0 +1,54 @@
+<?php
+
+class ShenbaoAction extends CommonAction{
+    function index(){
+    	
+    	$Model=M('Model');
+        $name="Download";
+        $map['catid'] = array('eq','27');
+        $map['table']=array('eq',$name);
+        $pageinfo=$Model->where($map)->find();
+
+        $Form   =   M($name);
+        import("@.ORG.Page");       //导入分页类
+        $count  = $Form->where($map)->count();    //计算总数
+        $Page = new Page($count, $pageinfo['listrows']);
+        $list   = $Form->where($map)->limit($Page->firstRow. ',' . $Page->listRows)->order('listorder,id desc')->select();
+        foreach ($list as &$value) {
+            $value['content'] = strip_tags($value['content']);
+        }
+        // 设置分页显示
+        $Page->setConfig('header', $pageinfo['header']);
+        $Page->setConfig('first', $pageinfo['first']);
+        $Page->setConfig('last', $pageinfo['last']);
+        $Page->setConfig('prev', $pageinfo['prev']);
+        $Page->setConfig('next', $pageinfo['next']);
+        $Page->setConfig('theme',$pageinfo['theme']);
+        $page = $Page->show();
+        $this->assign("page", $page);
+        $this->assign('data',$list);
+        $this->display();
+
+    }
+
+    function show(){
+
+        $id= I('id');
+        $name = "Download";
+        
+        D($name)->where('id='.$id)->setInc('hits',1);//浏览次数
+       
+        $model=M($name);
+
+        //当前记录
+        $data=$model->find($id);
+        $this->assign("data",$data);
+        $this->display();
+
+    }
+
+
+    
+}
+
+?>
---
+++ admin/Lib/Action/ApplicationformAction.class.php
@@ -0,0 +1,38 @@
+<?php
+class ApplicationFormAction extends CommonAction{
+	//查找申请表列表过滤
+	private function _filter(){
+		
+	}
+	
+	//查找申请表列表
+	public function select(){
+		
+	}
+	
+	//申请表格下载
+	public function download(){
+		
+	}
+	
+	//申请表格详情
+	public function show(){
+		
+	}
+	
+	//添加申请表格
+	public function add(){
+		$this->display();
+	}
+	
+	//上传申请表格附件
+	public function upload(){
+		
+	}
+
+	public function index(){
+		$this->display();
+	}
+
+}
+?>
